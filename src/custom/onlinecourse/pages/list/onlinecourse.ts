ad2a78d8b8c6aaa70e5cf586e98b3b37f51481b5
NguyenHoang <nhhoang1811vn@gmail.com>
2019-02-20 17:47:08 +0700

    - Implement more page UI only - Implement online course page

--- src/custom/onlinecourse/pages/list/onlinecourse.ts
+++ src/custom/onlinecourse/pages/list/onlinecourse.ts
@@ -1,17 +1,8 @@
-import { Component, ViewChild, ViewChildren } from '@angular/core';
+import { Component, ViewChild, ViewChildren, OnInit, OnDestroy } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-import { CoreUtilsProvider } from '@providers/utils/utils';
-import { AddonCourseCompletionProvider } from '@addon/coursecompletion/providers/coursecompletion';
-import { CoreCourseOptionsDelegate } from '@core/course/providers/options-delegate';
-import * as moment from 'moment';
-import { CoreDomUtilsProvider } from '@providers/utils/dom';
 import { CoreCourseHelperProvider } from '@core/course/providers/helper';
 import { CoreCoursesHelperProvider } from '@core/courses/providers/helper';
-import { CoreCoursesProvider } from '@core/courses/providers/courses';
-import { CustomApi } from '@providers/custom-api/custom-api';
-import { AddonBlockMyOverviewComponent } from '@addon/block/myoverview/components/myoverview/myoverview';
-import { CoreCoursesDashboardProvider } from '@core/courses/providers/dashboard';
+import { CoreTimeUtilsProvider } from '@providers/utils/time';
 
 
 @IonicPage()
@@ -20,28 +11,61 @@ import { CoreCoursesDashboardProvider } from '@core/courses/providers/dashboard'
   templateUrl: 'onlinecourse.html',
 })
 //This Page is copied from Course from Moodle
-export class OnlineCoursePage {
-    @ViewChild(AddonBlockMyOverviewComponent) addonBlockMyOverviewComponent: AddonBlockMyOverviewComponent
-    loaded = true
-    constructor(private dashboardProvider: CoreCoursesDashboardProvider){
+export class OnlineCoursePage implements OnInit{
+    ngOnInit(): void {
+        this.loadCourses(null);
+    }
+    loaded = false;
+    public courses : any[] = [];
+
+    constructor(private courseHelper: CoreCourseHelperProvider,
+        private coursesHelper: CoreCoursesHelperProvider,
+        private timeUtils: CoreTimeUtilsProvider
+        ){
 
     }
     ionViewDidLoad() {}
-    refreshDashboard(refresher: any): void {
+    loadCourses(refresher: any): void {
+        if (refresher!=null){
+            refresher.complete();
+        }
+        this.loaded = false;
         const promises = [];
-
-        promises.push(this.dashboardProvider.invalidateDashboardBlocks());
-
-        // Invalidate the blocks.
-        this.addonBlockMyOverviewComponent.invalidateContent().catch(() => {
-            // Ignore errors.
+        promises.push(this.fetchContent());
+        Promise.all(promises).finally(() => {            
+            this.loaded = true;
         });
-        
-
-        Promise.all(promises).finally(() => {
-            
-                refresher.complete();
+    }
+    
+    protected fetchContent(): Promise<any> {
+        return this.coursesHelper.getUserCoursesWithOptions("fullname").then((courses) => {
+            console.log("All course: " + courses.length);
+            this.courses = this.getInprogressCourses(courses);
+            console.log("Inprogress course: " + this.courses.length);
         });
+    }
+    protected getInprogressCourses(courses: any[]) : any[]{
+        let inprogressCourses = [];
+        const today = this.timeUtils.timestamp();
+        courses.forEach((course) => {
+            if (course.hidden) {
+                //this.courses.hidden.push(course);
+            } else  {
+                //this.courses.all.push(course);
+
+                if ((course.enddate && course.enddate < today) || course.completed) {
+                    // Courses that have already ended.
+                    //this.courses.past.push(course);
+                } else if (course.startdate > today) {
+                    // Courses that have not started yet.
+                    //this.courses.future.push(course);
+                } else {
+                    // Courses still in progress.
+                    inprogressCourses.push(course);
+                }
+            }
+        });
+        return inprogressCourses;
     }
     
 }
